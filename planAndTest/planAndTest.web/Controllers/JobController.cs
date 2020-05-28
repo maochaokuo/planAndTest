dae79611d79df06c3ac067561e064d3594af9b3e
Thomas Mao <tm1813@gmail.com>
2020-05-28 23:45:47 +0800

    bk

--- planAndTest/planAndTest.web/Controllers/JobController.cs
+++ planAndTest/planAndTest.web/Controllers/JobController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using planAndTest.web.Models.Job;
 
 namespace planAndTest.web.Controllers
 {
@@ -14,8 +15,32 @@ namespace planAndTest.web.Controllers
         }
         public IActionResult JobProgress()
         {
+            jProgress viewModel = new jProgress();
             //todo !!... keep updating running progress
-            return View();
+            //Response.CompleteAsynca.AddHeader("Refresh", "1");
+            viewModel.callIds.Add("call1");
+            viewModel.callIds.Add("call2");
+            viewModel.callDoneTodays.Add("call3");
+            viewModel.callDoneTodays.Add("call4");
+            viewModel.callDoneTodays.Add("call5");
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult JobProgress(jProgress viewModel)
+        {
+            viewModel.callIds.Add("call1");
+            viewModel.callIds.Add("call2");
+            viewModel.callDoneTodays.Add("call3");
+            viewModel.callDoneTodays.Add("call4");
+            viewModel.callDoneTodays.Add("call5");
+            switch(viewModel.cmd)
+            {
+                case "hello":
+                    break;
+                default:
+                    break;
+            }
+            return View(viewModel);
         }
     }
 }
